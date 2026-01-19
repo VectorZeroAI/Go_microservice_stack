1f2473bed752806d141253e94a669632bf68c864
Null <VectorZeroAI@gmail.com>
2026-01-19 18:36:50 +0100

    Logging system base complete

--- code/main.go
+++ code/main.go
@@ -9,7 +9,7 @@ import (
 
 // --------- Types creation  -------------
 // A singe log entry
-type Log_entry struct {
+type LogEntry struct {
 	App string
 	Action string
 	TimeStamp time.Time // The input will increment this.
@@ -17,21 +17,22 @@ type Log_entry struct {
 // per app log organisation
 type per_app_log struct {
 	App string
-	Logs []Log_entry
+	Logs []LogEntry
+	IsShutdown bool
 }
 // full log storage
 var FullLog []per_app_log
 
-// The input data structure
-type data struct {
+// The input IncommingString structure
+type IncommingString struct {
 	Application string `json:"application"`
 	Action_code int `json:"action_code"`
+	Event string `json:"Event"`
 }
 
 /*
 Map of action codes to actions:
 1. : start of a programm
-2. : init of logger
 3. : log an event
 4. : non_fatal error
 5. : fatal error
@@ -39,25 +40,56 @@ Map of action codes to actions:
 99. : log deletion
 */
 
+
 func postDataHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse the JSON body
-	var d data
-	err := json.NewDecoder(r.Body).Decode(&d);
+	var CurrentIncommingString IncommingString
+	err := json.NewDecoder(r.Body).Decode(&CurrentIncommingString);
 	if err != nil {
 		http.Error(w, "invalid JSON", http.StatusBadRequest)
 		return
 	}
 	// Now We check what the action code is, and act accordingly
-	if d.Action_code == 1 {
-		tmp := per_app_log{App: d.Application, Logs: []Log_entry{}}
-		current_log_entry := Log_entry{
-			App: d.Application,
+	if CurrentIncommingString.Action_code == 1 {
+		tmp := per_app_log{App: CurrentIncommingString.Application, Logs: []LogEntry{}, IsShutdown: false}
+		current_log_entry := LogEntry{
+			App: CurrentIncommingString.Application,
 			Action: "App started",
 			TimeStamp: time.Now(),
 		}
 		tmp.Logs = append(tmp.Logs, current_log_entry)
+		FullLog = append(FullLog, tmp)
+		return
 	}
-	
+	Log_string := func () {
+			tmp := LogEntry{
+				App: CurrentIncommingString.Application,
+				Action: CurrentIncommingString.Event,
+				TimeStamp: time.Now(),
+			}
+			for i := 0; i < len(FullLog); i++ {
+				if FullLog[i].App == CurrentIncommingString.Application {
+					FullLog[i].Logs = append(FullLog[i].Logs, tmp)
+					return
+				}
+			}
+		}
+	if CurrentIncommingString.Action_code == 3 {Log_string()}
+	if CurrentIncommingString.Action_code == 4 {Log_string()}
+	if CurrentIncommingString.Action_code == 5 {
+		tmp := LogEntry{
+			App: CurrentIncommingString.Application,
+			Action: CurrentIncommingString.Event,
+			TimeStamp: time.Now(),
+		}
+		for i := 0; i < len(FullLog); i++ {
+			if FullLog[i].App == CurrentIncommingString.Application {
+				FullLog[i].Logs = append(FullLog[i].Logs, tmp)
+				FullLog[i].IsShutdown = true
+				return
+			}
+		}
+	}
 }
 
 func main() {
